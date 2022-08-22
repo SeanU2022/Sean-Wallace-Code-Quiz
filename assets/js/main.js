cf5e5cd241c2f8f341ac394c8738e369fbb97217
Sean Wallace <sean.wallace.australia@gmail.com>
2022-08-23 09:54:48 +1000

    add countdown timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 // logic
+var startBtn = document.querySelector('#start-button');
+var submitBtn = document.getElementById('submit-button');
+
+// on START start timer
+var timeRemaining = 15;
+
+var timerEl = document.getElementById('countdown');
+
+function countdown(timeLeft) {    
+    var timeInterval = setInterval(function () {        
+        if (timeLeft > 1) {
+            timerEl.textContent = 'Time: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = 'Times Up!';
+            // stop the timer
+            clearInterval(timeLeft);
+        }
+    }, 1000);
+}
+
+
+
+startBtn.addEventListener('click', function() {
+    countdown(timeRemaining);
+});
+// countdown(timeRemaining);
